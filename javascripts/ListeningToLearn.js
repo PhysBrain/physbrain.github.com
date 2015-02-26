ee9bf2ed76d63390abd9dee4d0a8a20b52369fd2
PhysBrain <PhysBrain@gmail.com>
2015-02-25 21:23:11 -0600

    Updates to L2L

--- javascripts/ListeningToLearn.js
+++ javascripts/ListeningToLearn.js
@@ -1,6 +1,51 @@
+var currPage = 0;
+var currStep = [];
+var pages;
+var steps = [];
+
+function init() {
+    pages = document.querySelectorAll(".page");
+    for (var p=0; p<pages.length; ++p) {
+        steps.push(pages[p].querySelectorAll(".step"));
+        currStep.push(0);
+    }
+}
+
+function prevPage() {
+    if (currPage <= 0) return;
+    selectPage(currPage-1);
+}
+
+function nextPage() {
+    if (currPage >= pages.length-1) return;
+    selectPage(currPage+1);
+}
+
+function prevStep() {
+    steps[page][step].style.visibility = "hidden";
+    if (step[page] > 0) --step[page];
+}
+
+function nextStep() {
+    if (step > 0) --currstep;
+    steps[page][step].style.visibility = "hidden";
+}
+
 function selectPage(pageNum) {
-    var nodes = document.querySelectorAll(".page");
-    for (var i=0; i<nodes.length; ++i) {
-        nodes[i].style.visibility = ( (i+1)==pageNum ? "visible" :  "hidden" );
+    if (pageNum == page) {
+        nextStep();
+    }
+    else {
+        for (var i=0; i<steps[page].length; ++i) {
+            steps[page][i].style.visibility = "hidden";
+        }
+        pages[page].style.visibility = "hidden";
+
+        page = pageNum;
+
+        for (var i=0; i<steps[page].length; ++i) {
+            steps[page][i].style.visibility = "visible";
+        }
+        pages[page].style.visibility = "visible";
     }
 }
